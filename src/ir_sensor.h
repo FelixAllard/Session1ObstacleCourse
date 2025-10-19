7967c7f0c695c93b56cf5abdf259d7967b993388
Josh <GLAJ2848@>
2025-10-19 15:00:06 -0400

    IR_Sensor

---
+++ src/ir_sensor.h
@@ -0,0 +1,20 @@
+//
+// Created by joshg on 2025-10-19.
+//
+
+#ifndef IR_SENSOR_H
+#define IR_SENSOR_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+
+void IR_Init(void);
+
+// Retourne la distance en centimètres (10–80 cm)
+float IR_GetDistance(uint8_t id);
+
+// Retourne vrai si un obstacle est détecté à moins d’un seuil donné
+bool IR_IsDetected(uint8_t id, float seuil_cm);
+
+#endif
